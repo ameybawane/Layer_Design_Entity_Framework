96ea833728aba521f0a9e42f3aa208c715bbe716
Amey Bawane <60310813+ameybawane@users.noreply.github.com>
2022-10-31 15:48:09 +0530

    Add files via upload

---
+++ SMS_EF/SMS.Business/Abstraction/IStudentRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SMS.Business.Entities;
+
+namespace SMS.Business.Abstraction
+{
+    public interface IStudentRepository
+    {
+        public IEnumerable<Student> GetAllStudent();
+        public Student AddStu(Student student);
+        public int UpdateStu(int id, Student student);
+        public Student GetStuData(int id);
+        public Student DeleteStu(int id);
+    }
+}
